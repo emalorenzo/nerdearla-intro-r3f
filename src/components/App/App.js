9e4d2f2cb6c76ac681cb741b3f40db9a5fa2fb21
Emanuel Lorenzo <aemalorenzo@gmail.com>
2022-06-20 21:21:52 -0300

    Added little message pointing to repo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
+import { Message } from '../Message/Message';
 import './App.css';
 
 export const App = () => {
   return (
-    <div className='app-wrapper'>
+    <div className="app-wrapper">
+      <Message />
       Listos para empezar
     </div>
   );
-}
+};
