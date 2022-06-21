65ae9afdfd1ad7336b0c3361a7bcd1b9bd148ce1
Emanuel Lorenzo <aemalorenzo@gmail.com>
2022-06-20 21:21:35 -0300

    Added ESLint and Prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
-import { App }  from './components/App/App';
+import { App } from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,4 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
